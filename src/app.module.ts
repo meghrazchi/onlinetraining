a245103af284955b6e939d91596e8ec79845f105
Behzad Meghrazchi <berm0o0da11@gmail.com>
2022-03-23 20:50:56 +0430

    add section crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { VideoModule } from './video/video.module';
+import { SectionModule } from './section/section.module';
 
 @Module({
-  imports: [UserModule, VideoModule],
+  imports: [UserModule, VideoModule, SectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
