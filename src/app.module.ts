8c91e1d360aeacbd559bbab7ec9cf7d14e911cf8
Behzad Meghrazchi <berm0o0da11@gmail.com>
2022-03-23 20:53:07 +0430

    add tag crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { SectionModule } from './section/section.module';
 import { CourseModule } from './course/course.module';
 import { PackageModule } from './package/package.module';
 import { InstructorModule } from './instructor/instructor.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [UserModule, VideoModule, SectionModule, CourseModule, PackageModule, InstructorModule],
+  imports: [UserModule, VideoModule, SectionModule, CourseModule, PackageModule, InstructorModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
